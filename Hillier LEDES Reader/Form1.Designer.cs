3065654c121dde373cfdfe8ff435e0a30a16b094
Andy Hillier <84726034+CartographyLabs@users.noreply.github.com>
2022-02-19 11:13:04 -0500

    Add files via upload

---
+++ Hillier LEDES Reader/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace Hillier_LEDES_Reader
+{
+    partial class LEDESeditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LEDESeditor));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.tc1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.LEDESoutput = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.tc1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1283, 523);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tc1
+            // 
+            this.tc1.Controls.Add(this.tabPage1);
+            this.tc1.Controls.Add(this.tabPage2);
+            this.tc1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tc1.Location = new System.Drawing.Point(20, 86);
+            this.tc1.Name = "tc1";
+            this.tc1.SelectedIndex = 0;
+            this.tc1.Size = new System.Drawing.Size(1303, 564);
+            this.tc1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1295, 535);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Data Editor";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.LEDESoutput);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1295, 535);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "LEDES Output";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // LEDESoutput
+            // 
+            this.LEDESoutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.LEDESoutput.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LEDESoutput.ForeColor = System.Drawing.Color.White;
+            this.LEDESoutput.Location = new System.Drawing.Point(7, 8);
+            this.LEDESoutput.Multiline = true;
+            this.LEDESoutput.Name = "LEDESoutput";
+            this.LEDESoutput.Size = new System.Drawing.Size(1282, 521);
+            this.LEDESoutput.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(20, 22);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(87, 42);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load File";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(312, 22);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(87, 42);
+            this.btnWrite.TabIndex = 3;
+            this.btnWrite.Text = "Write File";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(1160, 22);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(163, 42);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close Application";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(129, 22);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(159, 42);
+            this.btnPreview.TabIndex = 6;
+            this.btnPreview.Text = "Preview Output";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // LEDESeditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1343, 662);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.tc1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "LEDESeditor";
+            this.Text = "LEDES 98B Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.tc1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tc1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.TextBox LEDESoutput;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnPreview;
+    }
+}
+
---
+++ Hillier LEDES Reader/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data;
+
+namespace Hillier_LEDES_Reader
+{
+    public partial class LEDESeditor : Form
+    {
+        string[] strData;
+        string[] strDataBody;
+
+        public LEDESeditor()
+        {
+            InitializeComponent();
+        }
+
+        public DataTable GenerateDataGridView(string[] LEDES)
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.AddRange(new DataColumn[24] { 
+                new DataColumn("INVOICE_DATE", typeof(string)),
+                new DataColumn("INVOICE_NUMBER", typeof(string)),
+                new DataColumn("CLIENT_ID", typeof(string)),
+                new DataColumn("LAW_FIRM_MATTER_ID", typeof(string)),
+                new DataColumn("INVOICE_TOTAL", typeof(string)),
+                new DataColumn("BILLING_START_DATE", typeof(string)),
+                new DataColumn("BILLING_END_DATE", typeof(string)),
+                new DataColumn("INVOICE_DESCRIPTION", typeof(string)),
+                new DataColumn("LINE_ITEM_NUMBER", typeof(string)),
+                new DataColumn("EXP/FEE/INV_ADJ_TYPE", typeof(string)),
+                new DataColumn("LINE_ITEM_NUMBER_OF_UNITS", typeof(string)),
+                new DataColumn("LINE_ITEM_ADJUSTMENT_AMOUNT", typeof(string)),
+                new DataColumn("LINE_ITEM_TOTAL", typeof(string)),
+                new DataColumn("LINE_ITEM_DATE", typeof(string)),
+                new DataColumn("LINE_ITEM_TASK_CODE",typeof(string)),
+                new DataColumn("LINE_ITEM_EXPENSE_CODE",typeof(string)),
+                new DataColumn("LINE_ITEM_ACTIVITY_CODE",typeof(string)),
+                new DataColumn("TIMEKEEPER_ID",typeof(string)),
+                new DataColumn("LINE_ITEM_DESCRIPTION",typeof(string)),
+                new DataColumn("LAW_FIRM_ID",typeof(string)),
+                new DataColumn("LINE_ITEM_UNIT_COST",typeof(string)),
+                new DataColumn("TIMEKEEPER_NAME",typeof(string)),
+                new DataColumn("TIMEKEEPER_CLASSIFICATION",typeof(string)),
+                new DataColumn("CLIENT_MATTER_ID[]",typeof(string))
+            });
+
+            string[] j;
+            foreach (string i in LEDES)
+            {
+                j = i.Split('|');
+                dt.Rows.Add(j);
+            }
+
+            return dt;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selectLEDES = new OpenFileDialog();
+            selectLEDES.InitialDirectory = @"C:\";
+
+            selectLEDES.Filter = "LED files (*.LED)|*.LED|Text Files (*.txt)|*.txt";
+
+            if (selectLEDES.ShowDialog() == DialogResult.OK)
+            {
+                strData = File.ReadAllLines(selectLEDES.FileName);
+                strDataBody = strData.Skip(2).ToArray();
+            }
+
+            DataTable dgvData = GenerateDataGridView(strDataBody);
+            dataGridView1.DataSource = dgvData;
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            LEDESoutput.Text = "LEDES1998B[]\r\n";
+
+            int cnt = dataGridView1.Columns.Count;
+            for (int i = 1; i <= cnt; i++)
+            {
+                int j = i - 1;
+                if (i < cnt)
+                {
+                    LEDESoutput.Text += dataGridView1.Columns[j].Name + "|";
+                }
+                else
+                {
+                    LEDESoutput.Text += dataGridView1.Columns[j].Name;
+                }
+            }
+            LEDESoutput.Text += "\r\n";
+            int cntRow = dataGridView1.Rows.Count;
+            for (int i = 1; i < cntRow; i++)
+            {
+                int cntCell = dataGridView1.Rows[i - 1].Cells.Count;
+                for (int j = 1; j <= cntCell; j++)
+                {
+                    int k = j - 1;
+                    if (j < cntCell)
+                    {
+                        LEDESoutput.Text += dataGridView1.Rows[i - 1].Cells[k].Value + "|";
+                    }
+                    else
+                    {
+                        LEDESoutput.Text += dataGridView1.Rows[i - 1].Cells[k].Value;
+                    }
+                }
+                LEDESoutput.Text += "\r\n";
+            }
+            /*foreach (DataGridViewRow i in dataGridView1.Rows)
+            {
+                int cRow = 0;
+                foreach (DataGridViewCell j in i.Cells)
+                {
+                    var cell = i.Cells[cRow].Value;
+                    int cCell = 1;
+                    if (cell != null)
+                    {
+                        if (cCell < i.Cells.Count-10)
+                        {
+                            LEDESoutput.Text += cell.ToString() + "|";
+                        }
+                        else
+                       {
+                            LEDESoutput.Text += cell.ToString();
+                        }
+                        cCell++;
+                    }
+                    cRow++;
+                }
+                LEDESoutput.Text += "\r\n";
+            }*/
+            tc1.SelectedTab = tabPage2;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog writeLEDES = new SaveFileDialog();
+            writeLEDES.InitialDirectory = @"C:\";
+            writeLEDES.Filter = "LEDES 98B file (*.LED)|*.LED|Text File (*.txt)|*.txt";
+
+            if (writeLEDES.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(writeLEDES.FileName, LEDESoutput.Text);
+            }
+        }
+    }
+}
